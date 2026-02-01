bf58419eabdeec35e0de83460314f3e5a519f414
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-01 22:19:42 +0100

    greeting link in welcome page added for authed users

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <h3 class="text-2xl font-bold">Home Page</h3>
+
+    @auth
+        <a href="{{ route('greeting') }}" class="block mt-4 px-2 py-3 bg-green-600 rounded-lg text-center cursor-pointer hover:bg-green-400">zu /greeting</a>
+    @endauth
 </x-layout>
